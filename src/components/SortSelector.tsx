95dadf356c70d2675f086130db6401d2ebe37123
adriandufac <adrian.dufac@gmail.com>
2024-02-20 09:51:11 +0100

    implemanting sorting games feature

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,18 +1,26 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 
-const SortSelector = () => {
+interface SortSelectorProps {
+    handleClick : (value : string) => void
+    selectedOrder: string 
+}
+const SortSelector = ({handleClick,selectedOrder} : SortSelectorProps) => {
+
+    const sortOrders = [
+        {display : "Relevance", value :""},
+        {display : "Date added", value : "-added"},
+        {display : "Name", value : "name"},
+        {display : "Release Date", value : "-released"},
+        {display : "Popularity", value : "-metacritic"},
+        {display : "Average Rating", value : "-rating"}
+    ]
  
     return (
         <Menu>
-            <MenuButton as={Button} rightIcon={<BsChevronDown/>}>Order by : Relevance</MenuButton>
+            <MenuButton as={Button} rightIcon={<BsChevronDown/>}>{"Order By " + (sortOrders.find((order) => order.value === selectedOrder)?.display || "Relevance")}</MenuButton>
             <MenuList>
-              <MenuItem>Relevance</MenuItem>
-              <MenuItem>Date added</MenuItem>
-              <MenuItem>Name</MenuItem>
-              <MenuItem>Release Date</MenuItem>
-              <MenuItem>Popularity</MenuItem>
-              <MenuItem>Average Rating</MenuItem>
+                {sortOrders.map(order => <MenuItem key={order.value} onClick={() =>handleClick(order.value)}> {order.display}</MenuItem>)}
             </MenuList>
         </Menu> 
       )
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,10 @@
 const getCroppedImageUrl = (url: string) => {
-    const target = 'media/'
-    const index = url.indexOf(target) + target.length;
-    const part1 = url.slice(0,index);
-    const part2 = url.slice(index);
-    return part1 + "crop/600/400/" + part2;
-}
+  if (!url) return "";
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  const part1 = url.slice(0, index);
+  const part2 = url.slice(index);
+  return part1 + "crop/600/400/" + part2;
+};
 
 export default getCroppedImageUrl;
