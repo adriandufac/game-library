aa59ca08184803e1334ae2b45d8ebf1c04eb37ab
adriandufac <adrian.dufac@gmail.com>
2024-02-21 11:56:42 +0100

    Making genres static data in our app because it almost never changes and doesnt need to be fetched everytime

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,7 @@
 import useDatas from "./useDatas";
 
+import genres from "../data/genres";
+
 export interface Genre {
   id: number;
   name: string;
@@ -8,5 +10,8 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => useDatas<Genre>("/genres");
+const useGenres = () => ({ datas: genres, isLoading: false, error: null });
+
+/** Use this if you want to dynamicly fetch genres from API */
+/* const useGenres = () => useDatas<Genre>("/genres"); */
 export default useGenres;
